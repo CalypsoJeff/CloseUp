60e3d150f94044a2d99644e5d732ee06530a5e8e
jeslinjayanvarghese <jeslinjaisan@gmail.com>
2023-07-27 18:26:29 +0530

    Closeup Website

---
+++ index.js
@@ -0,0 +1,11 @@
+function selectLocation(){
+    var location = document.getElementById('lang');
+    console.log(location.value);
+    alert("The location you have selected is " + location.value);
+    // window.location.href = "http://www.w3schools.com";
+
+}
+
+function myfunction(){
+    alert("Search button clicked");
+}
